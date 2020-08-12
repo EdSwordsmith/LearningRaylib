86febe171b0d90a07eb4382185393be87dfc68ae
Eduardo Espadeiro <eduardo.espadeiro@tecnico.ulisboa.pt>
2020-08-12 20:08:32 +0100

    Movement

--- ecs_test/main.cpp
+++ ecs_test/main.cpp
@@ -1,6 +1,8 @@
 #include "raylib.h"
 #include "entt/entt.hpp"
 
+using Velocity = Vector2;
+
 void CreateEntities(entt::registry &registry)
 {
     auto e1 = registry.create();
@@ -9,11 +11,36 @@ void CreateEntities(entt::registry &registry)
 
     auto e2 = registry.create();
     registry.emplace<Rectangle>(e2, Rectangle{400, 300, 50, 50});
+    registry.emplace<Velocity>(e2, Velocity{200, 0});
     registry.emplace<Color>(e2, RED);
 
     auto e3 = registry.create();
     registry.emplace<Rectangle>(e3, Rectangle{600, 100, 70, 70});
+    registry.emplace<Velocity>(e3, Velocity{0, 100});
     registry.emplace<Color>(e3, BLUE);
+}
+
+void MovementSystem(entt::registry &registry, float dt)
+{
+    auto view = registry.view<Rectangle, Velocity>();
+
+    for (auto entity : view)
+    {
+        auto &vel = view.get<Velocity>(entity);
+        auto &rect = view.get<Rectangle>(entity);
+
+        rect.x += vel.x * dt;
+        rect.y += vel.y * dt;
+
+        auto width = GetScreenWidth();
+        auto height = GetScreenHeight();
+
+        if (rect.x < -rect.width) rect.x = width - rect.width;
+        else if (rect.x > width) rect.x = 0;
+
+        if (rect.y < -rect.height) rect.y = height - rect.height;
+        else if (rect.y > height) rect.y = 0;
+    }
 }
 
 int main(void)
@@ -30,6 +57,9 @@ int main(void)
 
     while (!WindowShouldClose())
     {
+        float dt = GetFrameTime();
+        MovementSystem(registry, dt);
+
         BeginDrawing();
         ClearBackground(RAYWHITE);
 
@@ -38,6 +68,7 @@ int main(void)
         {
             auto &rect = view.get<Rectangle>(entity);
             auto &color = view.get<Color>(entity);
+
             DrawRectangleRec(rect, color);
         }
 
